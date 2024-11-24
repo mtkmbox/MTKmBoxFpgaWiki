a4faa78821152be65ea0da537394159ce77ed1c3
mtkmbox <mtkmbox@gmail.com>
2024-11-24 14:49:46 +0800

    DataPanel

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
-import './style/index.css'
+import './style/index.css';
+import DataPanel from "./components/DataPanel.vue";
+
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({app}) { 
+    // 注册全局组件
+    app.component('DataPanel' , DataPanel)
+  }
   // ...DefaultTheme, //或者这样写也可
 }
